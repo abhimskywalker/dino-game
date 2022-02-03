f1ef8f558fa79251ac3f2b788e4903a3c003dd4a
Abhishek Malik <malik.abhishek.iitk@gmail.com>
2022-02-03 08:11:37 +0530

    game now stops

--- index.js
+++ index.js
@@ -4,16 +4,30 @@ let game = document.getElementById('game-container')
 let character = document.getElementById('character')
 let obstacle = document.getElementById('obstacle')
 let gameScore = document.getElementById('game-score')
+let gameStatus = ''
 let isJumping = false
 let isCrossing = false
 let score = 0
+
+function gameStart() {
+	gameStatus = 'playing'
+	obstacle.classList.add("obstacle-animation")
+	score = 0
+}
+
+function gameEnd() {
+	character.classList.remove("character-jump")
+	obstacle.classList.remove("obstacle-animation")
+	gameStatus = 'lost'
+	alert('You lost!')
+}
 
 function jump() {
 	if (isJumping) {
 		return
 	}
-	if (obstacle.classList != "obstacle-animation") {
-		obstacle.classList.add("obstacle-animation")
+	if (!['playing', 'lost'].includes(gameStatus)) {
+		gameStart()
 	}
 	isJumping = true
 	character.classList.add('character-jump')
@@ -42,10 +56,10 @@ setInterval(() => {
 	let gapX = Math.abs(obstacleX - characterX)
 	let gapY = Math.abs(characterY - obstacleY)
 	// console.log('gapX:', gapX, ', gapY:', gapY)
-	if (gapX < 40 && gapY <= 50) {
-		characterDead()
-		// console.log('gapX:', gapX, ', gapY:', gapY)
-	} else if (gapX < 40 && gapY > 50 && !isCrossing) {
+	if (gapX < 35 && gapY <= 50) {
+		gameEnd()
+		console.log('gapX:', gapX, ', gapY:', gapY)
+	} else if (gapX < 35 && gapY > 50 && !isCrossing) {
 		isCrossing = true
 		score++
 		gameScore.textContent = 'Score: ' + score
@@ -53,10 +67,5 @@ setInterval(() => {
             isCrossing=false;
         }, 200);
 	}
-}, 100)
+}, 50)
 
-function characterDead() {
-	character.classList.remove("character-jump")
-	obstacle.classList.remove("obstacle-animation")
-	alert('You lost!')
-}
